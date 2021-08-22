ceeed14bf0bfbf7a04595785db80102758dcdf55
hgayan7 <hgayan7@gmail.com>
2021-08-22 20:52:13 +0530

    initialized project

---
+++ src/main/java/com/project/pickItUp/PickItUpApplication.java
@@ -0,0 +1,13 @@
+package com.project.pickItUp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickItUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PickItUpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/pickItUp/PickItUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.pickItUp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickItUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
